ae35e72db60771446d0cd121a49134f056b854e5
Th3R1c0 <ricots114@protonmail.com>
2022-04-30 22:36:24 -0700

    1 file modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,213 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
+const MainApp = () => {
+  return (
+    <div class="w-screen h-full items-center align-center flex flex-col">
+      <div className="w-screen h-1/3 flex justify-center items-center">
+        <h1 class=" w-max p-10 border-black border-b-2 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-500 to-red-600">
+          vote for the future
+        </h1>
+      </div>
+      <div class="flex items-center align-center justify-center content-center w-screen">
+        <div class="flex flex-col w-1/2 items-center">
+          <h1 class="m-2 w-max text-transparent font-bold text-3xl bg-clip-text bg-gradient-to-br from-purple-500 to-red-600">
+            Team pink
+          </h1>
+          <div>
+            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium   text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Team Pink
+              </span>
+            </button>
+          </div>
+          <div class="w-full h-full bg-red-200 flex flex-col justify-center items-center">
+            <p class="font-semibold text-md">Total votes: </p>
+            <h1 class="m-2 w-max text-transparent font-bold text-6xl bg-clip-text bg-gradient-to-br from-purple-500 to-red-600">
+              670
+            </h1>
+          </div>
+        </div>
+
+        <div class="flex flex-col w-1/2 items-center">
+          <h1 class=" m-2 w-max text-transparent font-bold text-3xl bg-clip-text bg-gradient-to-br from-cyan-500 to-blue-500">
+            Team Blue
+          </h1>
+          <div>
+            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Cyan to blue
+              </span>
+            </button>
+          </div>
+          <div class="w-full h-full bg-red-200 flex flex-col justify-center items-center">
+            <p class="font-semibold text-md">Total votes: </p>
+            <h1 class="m-2 w-max text-transparent font-bold text-6xl bg-clip-text bg-gradient-to-br from-cyan-500 to-blue-500">
+              220
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <div>
+      <div class="bg-red-100 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <div class="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="https://flowbite.com" class="flex items-center">
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              QQland
+            </span>
+          </a>
+          <div class="flex md:order-2">
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              random button
+            </button>
+            <button
+              data-collapse-toggle="mobile-menu-4"
+              type="button"
+              class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-4"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                class="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
+            id="mobile-menu-4"
+          >
+            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Stats = () => {
+  return (
+    <div class="w-screen h-max flex justify-center items-center">
+      <p> - total people voted: 200 - </p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div class="">
+      <footer class="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2022{' '}
+          <a href="https://flowbite.com" class="hover:underline">
+            Goofy ahhhh productions™
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0 ">
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">
+              Licensing
+            </a>
+          </li>
+          <li>
+            <a href="#" class="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>QQland</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div>QQland</div>
+      <div class="flex flex-col justify-between h-screen w-screen">
+        <Navbar />
+        <MainApp />
+        <Stats />
+        <Footer />
+      </div>
     </div>
   );
 };
